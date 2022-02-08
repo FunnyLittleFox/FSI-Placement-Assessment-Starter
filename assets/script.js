b33aca1de520be40e829798be3c7bf526f79cef5
Fox Flaherty <katyra14@gmail.com>
2022-02-07 20:46:58 -0800

    Believe I am ready for grading

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,14 @@ let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 let quantity = 0;
-
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
 
 
 function updateGb(displayQuantity){
     let gbQty = document.querySelector('#qty-gb');
-    gbQty.textContent = displayQuantity;    
+    gbQty.textContent = displayQuantity;
+
 }
 function updateCc(displayQuantity){
     let ccQty = document.querySelector('#qty-cc');
@@ -29,55 +29,68 @@ function updateTotal(displayQuantity){
     totalCookies.textContent = displayQuantity;
 }
 // selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
-const gbMinusBtn = document.querySelector('#minus-gb')
-const ccPlusBtn = document.querySelector('#add-cc')
-const ccMinusBtn = document.querySelector('#minus-cc')
-const sugarPlusBtn = document.querySelector('#add-sugar')
-const sugarMinusBtn = document.querySelector('#minus-sugar')
-
+const gbPlusBtn = document.querySelector('#add-gb');
+const gbMinusBtn = document.querySelector('#minus-gb');
+const ccPlusBtn = document.querySelector('#add-cc');
+const ccMinusBtn = document.querySelector('#minus-cc');
+const sugarPlusBtn = document.querySelector('#add-sugar');
+const sugarMinusBtn = document.querySelector('#minus-sugar');
 gbPlusBtn.addEventListener('click',function(e){
-    if (quantity >= 0) {
+    if (gb >= 0) {
+        gb++;
+        updateGb(gb);
+       (quantity >= 0)
         quantity++;
-        updateGb(quantity)
+        updateTotal(quantity);
     }
 })
 gbMinusBtn.addEventListener('click',function(e){
-    if (quantity > 0) {
+    if (gb > 0) {
+        gb--;
+        updateGb(gb);
+        (quantity > 0)
         quantity--;
-        updateGb(quantity)
-        updateTotal(totalCookies)
+        updateTotal(quantity);
     }
 })
 ccPlusBtn.addEventListener('click',function(e){
-    if (quantity >= 0) {
+    if (cc>= 0) {
+        cc++;
+        updateCc(cc);
+        (quantity >= 0)
         quantity++;
-        updateCc(quantity);
+        updateTotal(quantity);
     }
 })
 ccMinusBtn.addEventListener('click',function(e){
-    if (quantity > 0) {
+    if (cc> 0) {
+        cc--;
+        updateCc(cc);
+        (quantity > 0)
         quantity--;
-        updateCc(quantity);
+        updateTotal(quantity);
     }
 })
 sugarPlusBtn.addEventListener('click',function(e){
-    if (quantity >= 0) {
+    if (sugar>= 0) {
+        sugar++;
+        updateSugar(sugar);
+        (quantity >= 0)
         quantity++;
-        updateSugar(quantity);
+        updateTotal(quantity);
     }
 })
 sugarMinusBtn.addEventListener('click',function(e){
-    if (quantity > 0) {
+    if (sugar> 0) {
+        sugar--;
+        updateSugar(sugar);
+        (quantity > 0)
         quantity--;
-        updateSugar(quantity);
+        updateTotal(quantity);
     }
 })
-
-
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
-
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
 console.log('Gingerbread + button was clicked!')
@@ -97,7 +110,6 @@ sugarPlusBtn.addEventListener('click', function() {
 sugarMinusBtn.addEventListener('click', function() {
     console.log('Sugar - button was clicked!')
 })
-
 // HINT: You can delete this console.log after you no longer need it!
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 // TODO: Hook up event listeners for the rest of the buttons
